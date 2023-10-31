5477770e03889fb8ad7ee2b6386eb3ca802f6afd
Tirawat <98450270+Tirawat1@users.noreply.github.com>
2023-11-01 03:26:38 +0700

    Update create.vue

--- pages/product/create.vue
+++ pages/product/create.vue
@@ -62,11 +62,14 @@
                             </select>
                         </div>
                         <!-- Colors -->
-                        <div class="col-span-2 items-center justify-center">
-                            <select id="size" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg text-center focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <div class="col-span-1 items-center justify-center">
+                            <select id="color" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg text-center focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option selected></option>
                                 <option value="">red</option>
                             </select>
+                        </div>
+                        <div class="col-span-1 ">
+                            <input type="color" class="rounded-lg w-3/4">
                         </div>
                         <div class="col-span-2 items-center justify-center">
                             <input type="text" class="rounded-lg w-3/4" placeholder="amount">
