c2806846cf32c2346fcb57fe6244e6af46c5393d
kornavit <kornavit.p@ku.th>
2023-10-31 21:35:56 +0700

    add register and login

---
+++ pages/forget.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+let error_email = false;
+</script>
+
+<template>
+    <div class="md:flex">
+        <div class="md:basis-1/2 bg-white">
+            <div class="h-screen flex flex-col justify-center items-center" id="intro">
+                <p class="text-black text-3xl font-semibold tracking-wide mb-4">
+                    YOUR ACCOUNT FOR
+                    <br> 
+                    EVERYTHING MINNY.
+                </p>
+    
+                <p class="text-xs mb-16 font-semibold tracking-wide"> Welcome back! </p>
+                <form action="" class="w-[50%]" @submit.prevent="">
+                    <div class="mb-4">     
+                        <div class="relative">
+                            <input type="text" id="floating_outlined" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" />
+                            <label for="floating_outlined" class="absolute text-sm text-gray-500 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 left-1">Email</label>
+                        </div>
+                        <p v-if="error_email" id="outlined_error_help" class="mt-2 text-xs text-red-600"><span class="font-medium">Oh, snapp!</span> Some error message.</p>    
+                    </div>
+    
+                    <button type="submit" class="btn btn-info w-full text-white bg-[#2674B4] text-3xl">
+                        SUBMIT
+                    </button>
+                </form>
+                
+                <p class="mt-4 text-xs">Already a user? <nuxt-link to="login" class="text-[#2674B4]">Login</nuxt-link> </p>
+            </div>
+        </div>
+
+        <div class="hidden basis-1/2 md:flex items-center md:visible">
+            <img src="../assets/css/images/backgroundResgister.png" alt="" class="max-h-screen w-full">
+        </div>
+    </div>
+</template>
+
+<style scoped>
+::-webkit-calendar-picker-indicator {
+    filter: invert(1);
+}
+
+#intro{
+    animation-name: intro;
+    animation-duration: 1s;
+}
+
+@keyframes intro{
+    0% {
+    transform: translateY(25%);
+  }
+  100% {
+    transform: translateY(0);
+  }
+}
+
+
+</style>
--- store/pinia.store.ts
+++ store/pinia.store.ts
@@ -14,3 +14,20 @@ export const apiStore = defineStore('apiStore',() => {
 
     return { keep, getAll, get }
 })
+
+export const apiCountry = defineStore('apiProvince', () => {
+    
+    async function getProvince(){
+        return await useFetch("https://raw.githubusercontent.com/kongvut/thai-province-data/master/api_province.json")
+    }
+
+    async function getDistrict(){
+        return await useFetch("https://raw.githubusercontent.com/kongvut/thai-province-data/master/api_amphure.json")
+    }
+    
+    async function getSubdistrict(){
+        return await useFetch("https://raw.githubusercontent.com/kongvut/thai-province-data/master/api_tambon.json")
+    }
+    
+    return { getProvince ,getDistrict, getSubdistrict }
+})
