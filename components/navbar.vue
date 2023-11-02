07af1ac084a70da9d10b91b4ef0ad91fb0573d1f
Tirawat <98450270+Tirawat1@users.noreply.github.com>
2023-11-03 00:34:03 +0700

    add path to women

--- components/navbar.vue
+++ components/navbar.vue
@@ -84,7 +84,7 @@
         >
           <li>
             <a
-              href="#"
+              href="/category/women"
               class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
               aria-current="page"
               >Women</a
