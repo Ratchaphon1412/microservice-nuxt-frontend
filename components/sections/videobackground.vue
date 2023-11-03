1cc628f25f6231034eab054e5c0b321db4c577c7
saran <saran@MacBook-Air-khxng-saran.local>
2023-11-03 07:10:40 +0700

    update : landing Page

--- components/sections/videobackground.vue
+++ components/sections/videobackground.vue
@@ -1,28 +1,34 @@
 <template>
   <header class="relative flex items-center justify-center h-screen mb-12 overflow-hidden">
-    <video ref="videoPlayer" autoplay loop muted class="flex flex-col absolute z-10 w-auto min-w-full min-h-full max-w-none" :src="currentVideo.src" :type="currentVideo.type" @ended="playNextVideo">
+    <video autoplay loop muted class="flex flex-col absolute z-10 w-auto min-w-full min-h-full max-w-none" v-if="currentVideo" :key="currentVideo.src" :src="currentVideo.src" :type="currentVideo.type">
       Your browser does not support the video tag.
     </video>
   </header>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted, watchEffect } from 'vue';
 
 const fileSource = [
   // video1
-  { src: 'https://cdn.coverr.co/videos/coverr-girl-posing-on-the-edge-of-a-terrace-9533/1080p.mp4', type: 'video/mp4' },
+  { src: 'https://cdn.coverr.co/videos/coverr-girl-posing-on-the-edge-of-a-terrace-9533/1080p.mp4', type: 'video/mp4' } ,
   // video2
   { src: 'https://cdn.coverr.co/videos/coverr-two-people-walking-downstairs-quickly-7342/1080p.mp4', type: 'video/mp4' },
   // video3
   { src: 'https://cdn.coverr.co/videos/coverr--07-022-22-girl-with-analog-camera_0008-7692/1080p.mp4', type: 'video/mp4' },
+  { src: 'https://cdn.coverr.co/videos/coverr-woman-holding-a-dress-up-6788/1080p.mp4', type: 'video/mp4' },
+  
 ];
 
-const currentVideoIndex = ref(0);
+let currentVideoIndex = 0;
+const currentVideo = ref(fileSource[currentVideoIndex]);
 
-const playNextVideo = () => {
-  currentVideoIndex.value = (currentVideoIndex.value + 1) % fileSource.length;
+const changeVideo = () => {
+  currentVideoIndex = (currentVideoIndex + 1) % fileSource.length;
+  currentVideo.value = fileSource[currentVideoIndex];
 };
 
-const currentVideo = computed(() => fileSource[currentVideoIndex.value]);
+onMounted(() => {
+  setInterval(changeVideo, 10000); // Change video every 10 seconds
+});
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,14 +2,32 @@
   <main>
     <SectionsVideobackground />
     <h1 class="text-6xl font-bold text-start p-6">Trending Product</h1>
-    <div class="mx-auto max-w-2xl px-4 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
+
+      
+      <div class="carousel carousel-center p-4 w-full space-x-4 bg-neutral rounded-box">
+  <div class="carousel-item">
       <SectionsCard/>
+  </div> 
+  <div class="carousel-item">
       <SectionsCard/>
+  </div>
+  <div class="carousel-item">
       <SectionsCard/>
-    </div>
+  </div>
+  <div class="carousel-item">
+      <SectionsCard/>
+  </div>
+  <div class="carousel-item">
+      <SectionsCard/>
+  </div>
+  <div class="carousel-item">
+      <SectionsCard/>
+  </div>
+</div>
+
     <h1 class="text-6xl font-bold text-start p-6">Basic Product</h1>
     <div class="mx-auto max-w-2xl px-4 sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-24 lg:pt-16">
-      <div class="h-96 flex items-center justify-center ">
+  <div class="h-96 flex items-center justify-center transition duration-300 ease-in-out transform hover:scale-105">
         <div class="w-full h-full shadow-2xl overflow-hidden relative bg-black rounded-xl">
           <img class="absolute inset-0 h-full w-full object-cover opacity-50" src="https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="">
           <div class="flex flex-col h-full items-center justify-center relative">
@@ -17,7 +35,7 @@
           </div>
         </div>
       </div>
-      <div class="h-96 flex items-center justify-center ">
+      <div class="h-96 flex items-center justify-center transition duration-300 ease-in-out transform hover:scale-105">
         <div class="w-full h-full shadow-2xl overflow-hidden relative bg-black rounded-xl">
           <img class="absolute inset-0 h-full w-full object-cover opacity-50" src="https://images.unsplash.com/photo-1558769132-cb1aea458c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80" alt="">
           <div class="flex flex-col h-full items-center justify-center relative">
@@ -25,7 +43,7 @@
           </div>
         </div>
       </div>
-      <div class="h-96 flex items-center justify-center ">
+      <div class="h-96 flex items-center justify-center transition duration-300 ease-in-out transform hover:scale-105">
         <div class="w-full h-full shadow-2xl overflow-hidden relative bg-black rounded-xl">
           <img class="absolute inset-0 h-full w-full object-cover opacity-50" src="https://images.unsplash.com/photo-1495121605193-b116b5b9c5fe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1976&q=80" alt="">
           <div class="flex flex-col h-full items-center justify-center relative">
