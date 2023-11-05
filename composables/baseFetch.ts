edb8d0cc001b2c353f894ea3235e62412743798d
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-11-05 18:07:52 +0700

    edit useFetch

--- composables/baseFetch.ts
+++ composables/baseFetch.ts
@@ -1,11 +1,37 @@
-export default async function<T>(path:string ,options?:{}){
+
+
+
+type Headers = {
+  [key: string] :string
+}
+
+export default async function<T>(path:string ,options?:any){
+   
+    const {getAccessToken,isLogin,refreshToken} = useAuthStore()
     const config = useRuntimeConfig()
-    return await useFetch(path,{
-        ...options,
-        baseURL: config.public.apiBaseURL,
-        // baseURL: config.public.URL_ENDPOINT,
-        headers:{
-            "Accept": "application/json"
-        }
-    })
+
+    const headers : Headers = {
+        "Accept": "application/json",
+        
+      }
+
+      if (isLogin()) {
+        headers["Authorization"] = `Bearer ${getAccessToken()}`
+      }
+
+      
+
+
+    
+      
+         return await useFetch<T>(path,{
+            ...options,
+            baseURL: config.public.kongApi,
+            headers:{
+               ...headers,
+               ...options?.headers
+            },
+            
+        })
+     
 }
