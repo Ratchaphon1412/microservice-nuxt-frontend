5cada668e51dc0bca30fe91cf4ba42a3c41b4fbc
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-11-05 18:06:49 +0700

    add swith alert

--- app.vue
+++ app.vue
@@ -2,6 +2,9 @@
 import { onMounted } from "vue";
 import { initFlowbite } from "flowbite";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Swal from "sweetalert2/dist/sweetalert2.js";
+
+import "sweetalert2/src/sweetalert2.scss";
 // initialize components based on data attribute selectors
 onMounted(() => {
   initFlowbite();
