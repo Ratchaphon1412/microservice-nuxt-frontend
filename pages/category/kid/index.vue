237947cd6e1b0a6607875a6f8742ee812f87967e
Tirawat <98450270+Tirawat1@users.noreply.github.com>
2023-11-05 13:42:51 +0700

    categoryPage

--- pages/category/kid/index.vue
+++ pages/category/kid/index.vue
@@ -4,17 +4,17 @@
        <nav>
            <h1 class="p-6 text-6xl font-bold">All Cloth</h1>
            <div class="p-6">
-               <ul class="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
-                   <li class="w-full">
-                       <a href="#" aria-selected="true" class="inline-block w-full p-4 text-gray-900 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white" >Women</a>
-                   </li>
-                   <li class="w-full">
-                       <a href="/" aria-selected="false" class="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Men</a>
-                   </li>
-                   <li class="w-full">
-                       <a href="/" aria-selected="false" class="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Kid</a>
-                   </li>
-               </ul> 
+            <ul class="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow sm:flex dark:divide-gray-700 dark:text-gray-400">
+                <li class="w-full">
+                    <a href="/category/women" aria-selected="false" class="inline-block w-full p-4 text-gray-900 bg-gray-100 rounded-l-lg focus:ring-4 focus:ring-blue-300 active focus:outline-none dark:bg-gray-700 dark:text-white" >Women</a>
+                </li>
+                <li class="w-full">
+                    <a href="/category/men" aria-selected="false" class="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Men</a>
+                </li>
+                <li class="w-full">
+                    <a href="/category/kid" aria-selected="true" class="inline-block w-full p-4 bg-white hover:text-gray-700 hover:bg-gray-50 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700">Kid</a>
+                </li>
+            </ul>
            </div>
            <div id="indicators-carousel" class="p-6" data-carousel="static">
                <div class="relative h-56 overflow-hidden rounded-lg ">
