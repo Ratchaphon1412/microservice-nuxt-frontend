b36678d03d46d17ee5baa5a01adfbb4e523cf33a
eviperz <jirapa.wong@ku.th>
2023-11-05 15:13:07 +0700

    update filter from back to front

--- pages/category/women/index.vue
+++ pages/category/women/index.vue
@@ -247,7 +247,7 @@
        { name: 'red', class: '#cf1b3f', selectedClass: 'ring-gray-400' },
        { name: 'green', class: '#57ab3e', selectedClass: 'ring-gray-400' },
    ]
-   const sizes =['xss','xs','s','m','l','xl','2xl','3xl']
+   const sizes =['XSS','XS','S','M','L','XL','2XL','3XL']
    const listCost = [
                 {name: 'less than ฿500',cost:500},
                 {name: '฿500 - ฿999',cost:999},
@@ -255,7 +255,7 @@
             ]
 
    const { data: products } = await baseFetch<any>("product/format", {});
-   console.log(products.value.length);
+//    console.log(products.value.length);
 
 
    const filterList = ref<any>([])
@@ -271,7 +271,6 @@
         image : item.image,
     }})
 
-    console.log(filterList.value.length);
     const filterData = reactive({
         selectedColor: '',
         selectedSize: '',
@@ -301,8 +300,8 @@
             method: 'POST',
             body : filterData
         })
-        console.log(filter);
-        const filterList = ref<any>([]);
+        console.log(filter.value);
+        filterList.value = ref<any>([]);
         // const filterList = ref({
         //     name: '',
         //     description : '',
